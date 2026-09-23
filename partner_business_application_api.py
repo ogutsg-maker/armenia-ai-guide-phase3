2dbb8b0691f063064e48967476a2172dff7b822f
ogutsg-maker <ogutsg@gmail.com>
2026-09-23 14:41:29 +0400

    Fix admin applications 500 by joining application address

--- partner_business_application_api.py
+++ partner_business_application_api.py
@@ -605,6 +605,7 @@ def register_business_application_routes(app, bot_token=None, admin_id=None):
                      FROM partner_applications a
                      JOIN partners p ON p.id=a.partner_id
                      LEFT JOIN partner_businesses b ON b.id=a.business_id
+                     LEFT JOIN partner_objects po ON po.id=a.object_id
                      LEFT JOIN master_categories m ON m.id=a.master_category_id
                      LEFT JOIN categories c ON c.id=a.category_id
                      LEFT JOIN partner_verification_documents d ON d.id=a.document_id
